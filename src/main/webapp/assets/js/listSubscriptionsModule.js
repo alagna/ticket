19236041b587b43962e600d74d15bac5b48452ee
Alberto Lagna <alberto.lagna@whitebox.it>
2016-01-12 18:03:09 +0100

    listSubscriptions is working

--- src/main/webapp/assets/js/listSubscriptionsModule.js
+++ src/main/webapp/assets/js/listSubscriptionsModule.js
@@ -1,38 +1,17 @@
-var SINGLE_SEARCH_API_URL = './api/getExchangeRate';
-var MULTI_SEARCH_API_URL = './api/listExchangeRate';
-var CONVERT_API_URL = './api/convert';
+var SUBSCRIPTION_API_URL = './api/subscription/list';
 
-var app = angular.module('currencyApp', []);
-app.controller('currencyCtrl', function($scope,$http) {
+var app = angular.module('listSubscriptionsApp', []);
+
+app.controller('listSubscriptionsCtrl', function($scope,$http) {	
+	$scope.subscriptions=[];
 	
-    $scope.singleSearch = function() {
-		$scope.execSingleSearch = true;
-		$scope.waitSingleSearch = true;
-		$http.get(SINGLE_SEARCH_API_URL,{params:{currencyCode:$scope.currencyCode}})
-		.success(function(response) {
-			$scope.currencyExchange = response.currencyExchange;
-			$scope.waitSingleSearch = false;
+	$scope.listSubscriptions = function() {
+		console.log("calling listSubscriptions");
+		$http.get(SUBSCRIPTION_API_URL)
+			.success(function(response) {
+				console.log("got as response: " + JSON.stringify(response));
+				$scope.subscriptions=response.subscriptionList;
 		});		
 	};
-    $scope.multiSearch = function() {
-		$scope.execMultiSearch = true;
-		$scope.waitMultiSearch = true;
-		$http.get(MULTI_SEARCH_API_URL,{params:{currencyCodes:$scope.currencyCodes}})
-		.success(function(response) {
-			$scope.currencyExchanges = response.currencyExchanges;
-			$scope.waitMultiSearch = false;
-		});		
-    };
-    $scope.convert = function() {
-		$scope.execConvert = true;
-		$scope.waitConvert = true;
-		$http.get(CONVERT_API_URL,{params:{
-			sourceCurrencyCode:$scope.sourceCurrencyCode,
-			targetCurrencyCode:$scope.targetCurrencyCode,
-			amount:$scope.amount}})
-		.success(function(response) {
-			$scope.conversion = response.conversion;
-			$scope.waitConvert = false;
-		});		
-    };
+
 });
--- src/main/webapp/assets/js/sellSubscriptionsModule.js
+++ src/main/webapp/assets/js/sellSubscriptionsModule.js
@@ -3,13 +3,14 @@ var SUBSCRIPTION_API_URL = './api/subscription';
 var app = angular.module('sellSubscriptionsApp', []);
 
 app.controller('sellSubscriptionsCtrl', function($scope,$http) {	
-    
+	$scope.subscriptions=[];
+	
 	$scope.saveSubscription = function(subscription) {
 		console.log("saving subscription: " + JSON.stringify(subscription));
 		$http.post(SUBSCRIPTION_API_URL,subscription)
 			.success(function(response) {
 				console.log("got as response: " + JSON.stringify(response));
-				$scope.newSubscription = response.subscription;
+				$scope.subscriptions.push(response.subscription);
 		$scope.clearSubscription(subscription);
 		});		
 	};
