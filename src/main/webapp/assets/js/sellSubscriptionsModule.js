97c6abc781ee4ea3472b172c186b5bb8f7823c14
Alberto Lagna <alberto.lagna@whitebox.it>
2016-01-12 17:22:26 +0100

    createSubscription works

--- src/main/webapp/assets/js/sellSubscriptionsModule.js
+++ src/main/webapp/assets/js/sellSubscriptionsModule.js
@@ -2,13 +2,15 @@ var SUBSCRIPTION_API_URL = './api/subscription';
 
 var app = angular.module('sellSubscriptionsApp', []);
 
-app.controller('sellSubscriptionsCtrl', function($scope,$http) {
-    $scope.saveSubscription = function(subscription) {
+app.controller('sellSubscriptionsCtrl', function($scope,$http) {	
+    
+	$scope.saveSubscription = function(subscription) {
 		console.log("saving subscription: " + JSON.stringify(subscription));
 		$http.post(SUBSCRIPTION_API_URL,subscription)
-		.success(function(response) {
-			console.log("got as response: " + JSON.stringify(response));
-			subscription = response;
+			.success(function(response) {
+				console.log("got as response: " + JSON.stringify(response));
+				$scope.newSubscription = response.subscription;
+		$scope.clearSubscription(subscription);
 		});		
 	};
 	
