52e8e010a88184196c7ed040cb02f38e45b41b4c
Alberto Lagna <alberto.lagna@whitebox.it>
2016-01-15 12:18:54 +0100

    db path has to be absolute

--- bin/startDB.sh
+++ bin/startDB.sh
@@ -1,2 +1,2 @@
 echo "starting H2 DB"
-java -cp h2-1.4.190.jar org.h2.tools.Server -baseDir database
+java -cp h2-1.4.190.jar org.h2.tools.Server -baseDir /Users/alagna/Documents/lavoro/clienti/biznology/progetti/ticket/src/ticket-server/bin/database
