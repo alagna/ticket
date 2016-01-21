bfc6b4acbd60df32ca1e5d4bc67d0cf9780d83e8
Alberto Lagna <alberto.lagna@whitebox.it>
2016-01-21 11:21:10 +0100

    subscription added first and last name in separate fields

--- src/main/webapp/assets/js/sellSubscriptionsModule.js
+++ src/main/webapp/assets/js/sellSubscriptionsModule.js
@@ -8,6 +8,13 @@ app.controller('sellSubscriptionsCtrl', function($scope,$http) {
 	$scope.saveSubscription = function(subscription) {
 		var birthDate = new Date(subscription.subscriber.birthDate.replace( /(\d{2})\/(\d{2})\/(\d{4})/, "$3/$2/$1") );
 		subscription.subscriber.birthDate = birthDate.getTime();
+		
+		// merging first and last name
+		subscription.subscriber.firstLastName = 
+			subscription.subscriber.firstName.split() + " " + subscription.subscriber.lastName.split();
+		delete subscription.subscriber.firstName;
+		delete subscription.subscriber.lastName;
+		
 		console.log("saving subscription: " + JSON.stringify(subscription));
 		$http.post(SUBSCRIPTION_API_URL,subscription)
 			.success(function(response) {
