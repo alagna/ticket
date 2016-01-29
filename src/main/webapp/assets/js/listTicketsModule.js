305aa7f236d3e58a6c6a9b1f82bb4332348bbb47
Alberto Lagna <alberto.lagna@whitebox.it>
2016-01-29 11:58:00 +0100

    added the total amount of sold tickets

--- src/main/webapp/assets/js/listTicketsModule.js
+++ src/main/webapp/assets/js/listTicketsModule.js
@@ -3,6 +3,7 @@ var TICKET_API_URL = './api/purchase/list';
 var app = angular.module('listTicketsApp', []);
 
 app.controller('listTicketsCtrl', function($scope,$http) {	
+
 	var dateToString = function(date){
 		var dd = date.getDate(); 
 		var mm = date.getMonth()+1; 
@@ -21,12 +22,16 @@ app.controller('listTicketsCtrl', function($scope,$http) {
 	
 	var calcTicketList = function(startDate, endDate){
 		$scope.tickets=[];
+		$scope.totalAmount=0;
+		$scope.totalNumber=0;
 		$http.get(TICKET_API_URL, 
 				{params: {startDate: toMinus(startDate), endDate: toMinus(endDate)}})
 				.success(function(response) {
 					console.log("got as response: " + JSON.stringify(response));
 					for(var i=0; i<response.purchaseList.length; i++){
 						var purchase = response.purchaseList[i];
+						$scope.totalAmount=$scope.totalAmount+purchase.totalAmount;
+						$scope.totalNumber=$scope.totalNumber+purchase.ticketList.length;
 						for (var j=0; j<purchase.ticketList.length; j++)
 							$scope.tickets.push(purchase.ticketList[j]);
 					}
